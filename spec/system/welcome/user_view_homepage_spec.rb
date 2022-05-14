1bab5e8784deed6791dcb2563f947a7fe12623ea
Wilian Ferreira <wpaife@gmail.com>
2022-05-14 16:37:33 -0300

    Adiciona homepage ao projeto
    - Adiciona um layout simples para a fácil visualização

---
+++ spec/system/welcome/user_view_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Usuário visita a homepage' do
+  it 'e vê o título da aplicação' do
+    visit root_path
+
+    expect(current_path).to eq root_path
+    within('header') do 
+      expect(page).to have_css 'h1', text: 'Sistema de Entregas'
+    end
+  end
+end
