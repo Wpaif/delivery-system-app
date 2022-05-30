adef8f0d7939aac8868bf666d4bc6e112d1299dd
Wilian Ferreira <wpaife@gmail.com>
2022-05-30 13:11:52 -0300

    Ajusta a geração do código da ordem de serviço

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,13 @@ class Order < ApplicationRecord
   belongs_to :vehicle, optional: true
   belongs_to :carrier
 
-  validates :status, :recipient, :distance, :postal_code, :city,
-            :street, :number, :weight, :code, :carrier_id,
+  validates :status, :recipient, :distance, :postal_code, :city, :street, :number, :weight, :carrier_id,
             presence: true
 
-  validates :code, format: { with: /\A[0-9A-Z]{15}\z/ }
+  # validates :code, format: { with: /\A[0-9A-Z]{15}\z/ }
   validates :postal_code, format: { with: /\A[0-9]{5}-?[0-9]{3}\z/ }
 
-  before_validation :generate_code
+  before_create :generate_code
   before_update :set_estimated_delivery_date
 
   enum status: { pending: 0, on_carriage: 1, delivered: 2 }
